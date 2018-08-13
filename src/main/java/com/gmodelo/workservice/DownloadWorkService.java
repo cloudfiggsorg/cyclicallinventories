8186930a3dfc559ce16173cd400c19eed02bd5bf
CloudFiggsMac <2090399-cloudfiggs@gmail.com>
2018-08-13 11:18:20 -0500

    DownloadWork Service Request Info Tables

--- src/main/java/com/gmodelo/workservice/DownloadWorkService.java
+++ src/main/java/com/gmodelo/workservice/DownloadWorkService.java
@@ -11,32 +11,53 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.naming.InitialContext;
-import javax.naming.NameClassPair;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-
+import com.gmodelo.Exception.InvCicException;
 import com.gmodelo.beans.LoginBean;
 import com.gmodelo.beans.Request;
 import com.gmodelo.beans.Response;
+import com.gmodelo.beans.RfcTablesBean;
 import com.gmodelo.utils.ConnectionManager;
+import com.google.gson.Gson;
 
 public class DownloadWorkService {
 
 	Logger log = Logger.getLogger(getClass().getName());
 
-	//This Method Returns the 
-	public String GetInfoTablesWS(Request<LoginBean<?>> request) {
-		Response<?> response = new Response<>();
-		//CaFigueroa - Pending Code
-		
-		
-		return null;
+	/*
+	 * * * This Method Is used for the background data to the smartphone In
+	 * the @Request it will accept as follows, the @Return value is always a
+	 * List<RfcTableBean>
+	 * 
+	 * @ParamType1 @Null this will return all data in the table
+	 * 
+	 * @ParamType2 @List<RfcTableBean> this will return all the tables enumerated on
+	 * the table_name value in the RfcTableBean object contained in the list
+	 * 
+	 * @ParamType3 @RfcTableBean object, this object will pass the following filters
+	 * one or all may applies.
+	 * 
+	 * @ParamType3 @FilterValues #TABLE_NAME if you want only information for the
+	 * defined table.
+	 * 
+	 * @ParamType3 @FilterValues #DEVICE this will give you information of wich
+	 * tables will be downloaded to device.
+	 * 
+	 * @ParamType3 @FilterValues #LAST_UPDATE will give you the information that is
+	 * updated after the specified date.
+	 * 
+	 */
+	public String GetInfoTablesWS(Request<LoginBean<RfcTablesBean>> request) {
+		Response<List<RfcTablesBean>> response = new Response<>();
+		// CaFigueroa - Pending Code
+		List<RfcTablesBean> listToReturn = new ArrayList<>();
+		try {
+			listToReturn = new RfcTablesBean().RfcTablesBeanData(request.getLsObject());
+		} catch (InvCicException e) {
+			listToReturn = null;
+		}
+		return new Gson().toJson(response);
 	}
 
-	
-	
-	
 	public Response<List<Object>> GetMasterDataWork(Request<LoginBean<?>> request) {
 
 		log.log(Level.SEVERE, "Iniciando GetMasterDataWork");
