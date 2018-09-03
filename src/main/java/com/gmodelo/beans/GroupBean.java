e33f52c64004cfbe8f6f88fa2b4beef45a2bd6cc
Iris <Iris@IRISPC>
2018-09-03 17:35:30 -0500

    getGroups

--- src/main/java/com/gmodelo/beans/GroupBean.java
+++ src/main/java/com/gmodelo/beans/GroupBean.java
@@ -1,19 +1,38 @@
 package com.gmodelo.beans;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bmore.ume001.beans.User;
 
 public class GroupBean {
 	String groupId;
 	String groupDesc;
+	List<User> users;
 	
 	public GroupBean() {
 		super();
+		this.users= new ArrayList<User>();
 		// TODO Auto-generated constructor stub
 	}
 	
-	public GroupBean(String groupId, String groupDesc) {
+
+	public GroupBean(String groupId, String groupDesc, List<User> users) {
 		super();
 		this.groupId = groupId;
 		this.groupDesc = groupDesc;
+		this.users = users;
 	}
+	
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
 
 	public String getGroupId() {
 		return groupId;
@@ -28,9 +47,11 @@ public class GroupBean {
 		this.groupDesc = groupDesc;
 	}
 
+
 	@Override
 	public String toString() {
-		return "GroupBean [groupId=" + groupId + ", groupDesc=" + groupDesc + "]";
+		return "GroupBean [groupId=" + groupId + ", groupDesc=" + groupDesc + ", users=" + users + "]";
 	}
+
 	
 }
