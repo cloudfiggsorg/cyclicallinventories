5408f3baf8742e89536c037e7c0f0f839c2576e2
Rodrigo Cervantes <rdgo_cbs@hotmail.com>
2018-09-22 00:46:24 -0500

    updates

--- src/main/java/com/gmodelo/beans/DocInvBean.java
+++ src/main/java/com/gmodelo/beans/DocInvBean.java
@@ -7,51 +7,15 @@ public class DocInvBean {
 	String werks;
 	String werksD;
 	String type;
+	String status;
 	String createdBy;
 	int docInvId;
-
+	
 	public String getRoute() {
 		return route;
 	}
-
 	public void setRoute(String route) {
 		this.route = route;
-	}
-
-	public DocInvBean() {
-		super();
-	}
-	
-	public String getBukrsD() {
-		return bukrsD;
-	}
-
-	public void setBukrsD(String bukrsD) {
-		this.bukrsD = bukrsD;
-	}
-
-	public String getWerksD() {
-		return werksD;
-	}
-
-	public void setWerksD(String werksD) {
-		this.werksD = werksD;
-	}
-	
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getWerks() {
-		return werks;
-	}
-
-	public void setWerks(String werks) {
-		this.werks = werks;
 	}
 	public String getBukrs() {
 		return bukrs;
@@ -59,7 +23,36 @@ public class DocInvBean {
 	public void setBukrs(String bukrs) {
 		this.bukrs = bukrs;
 	}
-	
+	public String getBukrsD() {
+		return bukrsD;
+	}
+	public void setBukrsD(String bukrsD) {
+		this.bukrsD = bukrsD;
+	}
+	public String getWerks() {
+		return werks;
+	}
+	public void setWerks(String werks) {
+		this.werks = werks;
+	}
+	public String getWerksD() {
+		return werksD;
+	}
+	public void setWerksD(String werksD) {
+		this.werksD = werksD;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	public String getCreatedBy() {
 		return createdBy;
 	}
@@ -72,9 +65,9 @@ public class DocInvBean {
 	public void setDocInvId(int docInvId) {
 		this.docInvId = docInvId;
 	}
-
+	
 	public DocInvBean(String route, String bukrs, String bukrsD, String werks, String werksD, String type,
-			String createdBy, int docInvId) {
+			String status, String createdBy, int docInvId) {
 		super();
 		this.route = route;
 		this.bukrs = bukrs;
@@ -82,15 +75,24 @@ public class DocInvBean {
 		this.werks = werks;
 		this.werksD = werksD;
 		this.type = type;
+		this.status = status;
 		this.createdBy = createdBy;
 		this.docInvId = docInvId;
 	}
-
+	
+	
+	
+	public DocInvBean() {
+		super();
+	}
 	@Override
 	public String toString() {
 		return "DocInvBean [route=" + route + ", bukrs=" + bukrs + ", bukrsD=" + bukrsD + ", werks=" + werks
-				+ ", werksD=" + werksD + ", type=" + type + ", createdBy=" + createdBy + ", docInvId=" + docInvId + "]";
+				+ ", werksD=" + werksD + ", type=" + type + ", status=" + status + ", createdBy=" + createdBy
+				+ ", docInvId=" + docInvId + "]";
 	}
+
+	
 
 	
 	
