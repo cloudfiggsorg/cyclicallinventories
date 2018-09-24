e4774a7f74490808b07b05c5076035b1e26e1cca
Iris <Iris@192.168.1.69>
2018-09-24 12:10:51 -0500

    ConciliacionDao

--- src/main/java/com/gmodelo/dao/ConciliacionDao.java
+++ src/main/java/com/gmodelo/dao/ConciliacionDao.java
@@ -1,10 +1,12 @@
 package com.gmodelo.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLWarning;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -13,7 +15,7 @@ import java.util.logging.Logger;
 import com.gmodelo.beans.AbstractResultsBean;
 import com.gmodelo.beans.ConciliacionBean;
 import com.gmodelo.beans.ConciliationsIDsBean;
-import com.gmodelo.beans.DocInvPositionBean;
+import com.gmodelo.beans.ConciliationPositionBean;
 import com.gmodelo.beans.Response;
 import com.gmodelo.utils.ConnectionManager;
 import com.gmodelo.utils.ReturnValues;
@@ -101,8 +103,9 @@ public class ConciliacionDao {
 				INV_VW_DOC_INV += condition;
 			}
 		}
-		log.info(INV_VW_DOC_INV);
+		
 		INV_VW_DOC_INV += " GROUP BY DOC_INV_ID, ROUTE_ID, BUKRS, BDESC, WERKS, WERKSD, TYPE,JUSTIFICATION";
+		log.info(INV_VW_DOC_INV);
 		log.info("[getDocInvDao] Preparing sentence...");
 		
 		try {
@@ -119,7 +122,7 @@ public class ConciliacionDao {
 				docInvBean.setWerks(rs.getString("WERKS"));
 				docInvBean.setWerksD(rs.getString("WERKSD"));
 				docInvBean.setJustification(rs.getString("JUSTIFICATION"));
-				docInvBean.setPositions(null);//PENDIENTE poner positions de ConciliationPositionBean
+				docInvBean.setPositions(getPositions(rs.getInt("DOC_INV_ID")));
 				listDocInv.add(docInvBean);
 			}
 			
@@ -154,56 +157,78 @@ public class ConciliacionDao {
 		return res ;
 	}
 	
-	private List<DocInvPositionBean> getPositions(String docInvId){
+	private List<ConciliationPositionBean> getPositions(int i){
 		ConnectionManager iConnectionManager = new ConnectionManager();
 		Connection con = iConnectionManager.createConnection();
+		CallableStatement cs = null;
 		PreparedStatement stm = null;
-		List<DocInvPositionBean> listPositions = new ArrayList<DocInvPositionBean>();
-		String INV_VW_DOC_INVENTORY_POSITIONS = "SELECT LGORT,LGTYP,LGPLA,MATNR,THEORIC,COUNTED,DIFF_COUNTED,DIFF_FLAG,DOC_INV_ID,LTYPT,GDES,MAKTX  FROM INV_VW_DOC_INVENTORY_POSITIONS WITH(NOLOCK) WHERE DOC_INV_ID = ?";
-		log.info(INV_VW_DOC_INVENTORY_POSITIONS);
-		log.info("getPositionsDocInvDao] Preparing sentence...");
-		INV_VW_DOC_INVENTORY_POSITIONS += " GROUP BY LGORT,LGTYP,LGPLA,MATNR,THEORIC,COUNTED,DIFF_COUNTED,DIFF_FLAG,DOC_INV_ID,LTYPT,GDES,MAKTX";
+		ResultSet rs = null;
+		List<ConciliationPositionBean> listPositions = new ArrayList<ConciliationPositionBean>();
+		
+		String DROP_TABLE = "INV_SP_DROP_TABLE_COUNT_POSITIONS ?";
+		String INV_VW_DOC_INVENTORY_POSITIONS_TEMP = "SELECT TAS_DOC_INV_ID, ZONE_ID, ZONE_DESC, LGPLA, MATNR,  MATDESC, MEINS, COUNT_NUM, TOTAL INTO DBO.COUNT_POSITIONS FROM INV_VW_COUNT_POSITIONS_BASE WITH(NOLOCK) WHERE TAS_DOC_INV_ID=? ";
+		INV_VW_DOC_INVENTORY_POSITIONS_TEMP += " ORDER BY MATNR,COUNT_NUM ASC";
+		
+		String INV_VW_COUNT_POSITIONS = "SELECT TAS_DOC_INV_ID, ZONE_ID, ZONE_DESC,LGPLA, MATNR, MATDESC, MEINS, [1A],[1B],[2],[3] FROM INV_VW_COUNT_POSITIONS ORDER BY MATNR ASC ";
 		try {
-			stm = con.prepareCall(INV_VW_DOC_INVENTORY_POSITIONS);
-			stm.setString(1, docInvId);
-			log.info("[getPositionsDocInvDao] Executing query...");
-			ResultSet rs = stm.executeQuery();
-			while (rs.next()){
-				DocInvPositionBean position = new DocInvPositionBean();
-				position.setDocInvId(Integer.parseInt(docInvId));
-				position.setLgort(rs.getInt("LGORT"));
-				position.setLgtyp(rs.getString("LGTYP"));
-				position.setLgpla(rs.getString("LGPLA"));
-				position.setMatnr(rs.getString("MATNR"));
-				position.setTheoric(rs.getString("THEORIC"));
-				position.setCounted(rs.getString("COUNTED"));
-				position.setDiffCounted(rs.getString("DIFF_COUNTED"));
-				position.setFlag(rs.getString("DIFF_FLAG"));
-				position.setLgtypDes(rs.getString("LTYPT"));
-				position.setGdes(rs.getString("GDES"));
-				position.setMatnrDes(rs.getString("MAKTX"));
-				listPositions.add(position);
-			}
 			
-			//Retrive the warnings if there're
-			SQLWarning warning = stm.getWarnings();
-			while (warning != null) {
-				log.log(Level.WARNING,warning.getMessage());
-				warning = warning.getNextWarning();
+			//DROP TABLE TEMPORAL CONCILIATION POSITIONS
+			cs = con.prepareCall(DROP_TABLE);
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.execute();
+			if(cs.getInt(1) > 0){
+				
+				//GENERAR TABLE TEMPORAL
+				stm = con.prepareCall(INV_VW_DOC_INVENTORY_POSITIONS_TEMP);
+				stm.setInt(1, i);
+				System.out.println("id: "+ i);
+				log.info("[getPositionsConciliationDao] TABLE_CONCILIATION_POSITIONS Temp, Executing query...");
+				int result = stm.executeUpdate();
+				System.out.println("result:" +result);
+				if(result > 0){
+					//EJECUTAR TABLE FINAL TABLE_CONCILIATION_POSITIONS
+					stm = null;
+					stm = con.prepareCall(INV_VW_COUNT_POSITIONS);
+					rs = stm.executeQuery();
+					if(rs != null){
+						while (rs.next()){
+							ConciliationPositionBean position = new ConciliationPositionBean();
+							position.setZoneId(rs.getString("ZONE_ID"));   
+							position.setZoneD(rs.getString("ZONE_DESC"));
+							position.setLgpla(rs.getString("LGPLA"));
+							position.setMatnr(rs.getString("MATNR"));
+							position.setMatnrD(rs.getString("MATDESC"));
+							position.setMeasureUnit(rs.getString("MEINS"));
+							position.setCount1A(rs.getString("1A"));
+							position.setCount1B(rs.getString("1B"));
+							position.setCount2(rs.getString("2"));
+							position.setCount3(rs.getString("3"));
+							listPositions.add(position);
+						}
+					}else{
+						log.info("[getPositionsConciliationDao] No data in TABLE_CONCILIATION_POSITIONS Final...");
+					}
+				}else{
+					log.info("[getPositionsConciliationDao] No data in TABLE_CONCILIATION_POSITIONS Temp...");
+				}
+				
+			}else{
+				log.info("getPositionsConciliationDao] Not delete table Temp");
 			}
 			
 			//Free resources
-			rs.close();
-			stm.close();	
+			if(cs != null){cs.close();}
+			if(rs != null){rs.close();}
+			if(stm != null){stm.close();}	
 			
-			log.info("[getPositionsDocInvDao] Sentence successfully executed.");
+			log.info("[getPositionsConciliationDao] Sentence successfully executed.");
 		} catch (SQLException e) {
-			log.log(Level.SEVERE,"[getPositionsDocInvDao] Some error occurred while was trying to execute the query: "+INV_VW_DOC_INVENTORY_POSITIONS, e);
+			log.log(Level.SEVERE,"[getPositionsConciliationDao] Some error occurred while was trying to execute the query: "+INV_VW_DOC_INVENTORY_POSITIONS_TEMP, e);
 		}finally {
 			try {
 				con.close();
 			} catch (SQLException e) {
-				log.log(Level.SEVERE,"[getPositionsDocInvDao] Some error occurred while was trying to close the connection.", e);
+				log.log(Level.SEVERE,"[getPositionsConciliationDao] Some error occurred while was trying to close the connection.", e);
 			}
 		}
 		return listPositions;
@@ -232,4 +257,16 @@ public class ConciliacionDao {
 		return condition;
 	}
 
+	/*
+	public static void main(String args[]){
+		ConciliacionDao dao = new ConciliacionDao();
+		ConciliacionBean docInvBean = new ConciliacionBean();
+		docInvBean.setDocInvId(3);
+		String searchFilter = "";
+		Response<List<ConciliacionBean>> x = dao.getConciliacion(docInvBean, searchFilter);
+		for(int i=0; i < x.getLsObject().size(); i++){
+			System.out.println(x.getLsObject().get(i).toString());
+		}
+	}
+	*/
 }
