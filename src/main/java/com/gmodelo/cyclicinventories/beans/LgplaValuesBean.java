bfbb85e570d353a6721b270c6aadd1ae156e9511
Rodrigo Cervantes <rdgo_cbs@hotmail.com>
2018-12-21 13:26:57 -0600

    lgplavaluesbean add

--- src/main/java/com/gmodelo/cyclicinventories/beans/LgplaValuesBean.java
+++ src/main/java/com/gmodelo/cyclicinventories/beans/LgplaValuesBean.java
@@ -8,10 +8,14 @@ public class LgplaValuesBean {
 	private String sec;
 	private String tarimas;
 	private String camas;
-	private String um;// Cantidad de Unidad de medida
+	private String um;// Cantidad de Unidad de medida osea caja
 	private String totalConverted;
 	private String materialNotes;
 	private String prodDate;
+	private String vhilmQuan;
+	private String cpc;
+	private String cpp;
+	
 	private boolean locked;
 	private long dateStart;
 	private long dateEnd;
@@ -129,17 +133,44 @@ public class LgplaValuesBean {
 		this.prodDate = prodDate;
 	}
 
+	public String getVhilmQuan() {
+		return vhilmQuan;
+	}
+
+	public void setVhilmQuan(String vhilmQuan) {
+		this.vhilmQuan = vhilmQuan;
+	}
+
+	public String getCpc() {
+		return cpc;
+	}
+
+	public void setCpc(String cpc) {
+		this.cpc = cpc;
+	}
+
+	public String getCpp() {
+		return cpp;
+	}
+
+	public void setCpp(String cpp) {
+		this.cpp = cpp;
+	}
+
+	
+
 	@Override
 	public String toString() {
-		return "LgplaValuesBean [matnr=" + matnr + ", vhilm=" + vhilm + ", maktx=" + matkx + ", sec=" + sec
+		return "LgplaValuesBean [matnr=" + matnr + ", vhilm=" + vhilm + ", matkx=" + matkx + ", sec=" + sec
 				+ ", tarimas=" + tarimas + ", camas=" + camas + ", um=" + um + ", totalConverted=" + totalConverted
-				+ ", materialNotes=" + materialNotes + ", prodDate=" + prodDate + ", locked=" + locked + ", dateStart="
-				+ dateStart + ", dateEnd=" + dateEnd + "]";
+				+ ", materialNotes=" + materialNotes + ", prodDate=" + prodDate + ", vhilmQuan=" + vhilmQuan + ", cpc="
+				+ cpc + ", cpp=" + cpp + ", locked=" + locked + ", dateStart=" + dateStart + ", dateEnd=" + dateEnd
+				+ "]";
 	}
 
 	public LgplaValuesBean(String matnr, String vhilm, String matkx, String sec, String tarimas, String camas,
-			String um, String totalConverted, String materialNotes, String prodDate, boolean locked, long dateStart,
-			long dateEnd) {
+			String um, String totalConverted, String materialNotes, String prodDate, String vhilmQuan, String cpc,
+			String cpp, boolean locked, long dateStart, long dateEnd) {
 		super();
 		this.matnr = matnr;
 		this.vhilm = vhilm;
@@ -151,9 +182,14 @@ public class LgplaValuesBean {
 		this.totalConverted = totalConverted;
 		this.materialNotes = materialNotes;
 		this.prodDate = prodDate;
+		this.vhilmQuan = vhilmQuan;
+		this.cpc = cpc;
+		this.cpp = cpp;
 		this.locked = locked;
 		this.dateStart = dateStart;
 		this.dateEnd = dateEnd;
 	}
+
+	
 
 }
